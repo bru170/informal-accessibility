91197e59dffef0f6f152246ad7b0abb306b24d3d
bru170 <80203072+bru170@users.noreply.github.com>
2021-11-21 20:38:54 +0100

    added logo, rmeoved unused imports

--- src/components/Navigation/Hamburger/Hamburger.js
+++ src/components/Navigation/Hamburger/Hamburger.js
@@ -1,8 +1,14 @@
 import React from "react"
-import {MenuOpenIcon, MenuCloseIcon} from "./Hamburger.styles"
+import {MenuOpenIcon} from "./Hamburger.styles"
 
-const Hamburger = ({handleOverlayMenu, menuOpen}) => {
-  return <>{MenuOpenIcon && <MenuOpenIcon aria-label="open" onClick={handleOverlayMenu} />}</>
+const Hamburger = ({handleOverlayMenu}) => {
+  return (
+    <>
+      {MenuOpenIcon && (
+        <MenuOpenIcon aria-label="opens navbar menu for mobile" onClick={handleOverlayMenu} />
+      )}
+    </>
+  )
 }
 
 export default Hamburger
--- src/components/Navigation/Hamburger/Hamburger.styles.js
+++ src/components/Navigation/Hamburger/Hamburger.styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai"
+import {AiOutlineMenu} from "react-icons/ai"
 
 export const MenuOpenIcon = styled(AiOutlineMenu)`
   transform: scale(1);
@@ -9,6 +9,7 @@ export const MenuOpenIcon = styled(AiOutlineMenu)`
   width: 40px;
   padding-top: 20px;
   height: 40px;
+  color: #bb3355;
   cursor: pointer;
 
   @media (min-width: 800px) {
@@ -17,25 +18,14 @@ export const MenuOpenIcon = styled(AiOutlineMenu)`
 
   &:hover {
     color: #bb3355;
+    border: 2px solid #bb3355;
+  }
+
+  &:focus {
+    border: 2px solid #bb3355;
+  }
+
+  &:active {
+    border: 2px solid #bb3355;
   }
 `
-
-// export const MenuCloseIcon = styled(AiOutlineClose)`
-//   transform: scale(0.75);
-//   position: fixed;
-//   display: block;
-//   z-index: 10001;
-//   right: 10px;
-//   top: 1px;
-//   width: 40px;
-//   height: 40px;
-//   cursor: pointer;
-
-//   @media (min-width: 800px) {
-//     display: none;
-//   }
-
-//   &:hover {
-//     color: #bb3355;
-//   }
-// `
