903c5afeb887586781feea2315a86aab64d721f9
bru170 <80203072+bru170@users.noreply.github.com>
2022-01-01 23:51:57 +0000

    fixed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,10 +7,15 @@ export const Wrapper = styled.div`
   max-width: 1000px;
   max-height: auto;
   margin: auto;
+  padding-bottom: 100px !important;
   padding-top: 50px;
 
   h1 {
     color: black !important;
+  }
+
+  @media (max-width: 1367px) {
+    margin-bottom: 50px;
   }
 `
 
@@ -20,11 +25,14 @@ const NotFoundPage = () => (
       <Wrapper>
         <h1>404: Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist..mmmm. the sadness.</p>
-        <Image
+
+        <StaticImage
           src="../images/error404.jpg"
           alt="A women looking at her phone confused because it says 404"
           placeholder="blurred"
           layout="fixed"
+          width={1000}
+          height={700}
         />
       </Wrapper>
     </Layout>
