e191e52a1e40231d5cc18f52e031e60ffcffed85
bru170 <80203072+bru170@users.noreply.github.com>
2022-01-01 23:20:54 +0000

    ss

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,24 @@
 import * as React from "react"
+import styled from "styled-components"
 import Layout from "../components/Layout/Layout"
+
+export const Wrapper = styled.div`
+  max-width: 1000px;
+  max-height: auto;
+  margin: auto;
+  padding-top: 50px;
+
+  h1 {
+    color: black !important;
+  }
+`
 
 const NotFoundPage = () => (
   <>
-    <Layout>
+    <Wrapper>
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+      <p>You just hit a route that doesn&#39;t exist..mmmm. the sadness.</p>
+    </Wrapper>
   </>
 )
 
