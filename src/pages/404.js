3b497258d59d56829cad148e67588d8fd6a075fd
bru170 <80203072+bru170@users.noreply.github.com>
2022-01-01 23:29:01 +0000

    with image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import styled from "styled-components"
+import {StaticImage} from "gatsby-plugin-image"
 import Layout from "../components/Layout/Layout"
 
 export const Wrapper = styled.div`
@@ -19,6 +20,10 @@ const NotFoundPage = () => (
       <Wrapper>
         <h1>404: Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist..mmmm. the sadness.</p>
+        <StaticImage
+          src="src/images/error 404.jpg"
+          alt="A women looking at her phone confused because it says 404"
+        />
       </Wrapper>
     </Layout>
   </>
