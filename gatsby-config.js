5f151c90f6e4a862b7236c7a5ee77492637bdc13
bru170 <80203072+bru170@users.noreply.github.com>
2022-01-31 17:18:22 +0100

    changed back to pantheon

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        url: `https://admin-soswelfare.space/graphql`
+        url: `https://dev-digital-welfare.pantheonsite.io/graphql`
       }
     },
     `gatsby-plugin-styled-components`,
--- src/hooks/useAboutQuery.js
+++ src/hooks/useAboutQuery.js
@@ -3,7 +3,7 @@ import {useStaticQuery, graphql} from "gatsby"
 export const useAboutQuery = () => {
   const data = useStaticQuery(graphql`
     query MyQuery {
-      wpPage(databaseId: {eq: 8}) {
+      wpPage(databaseId: {eq: 10}) {
         content
         featuredImage {
           node {
