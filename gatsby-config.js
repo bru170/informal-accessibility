36bff30b5ab764e4bf0461d8b3e719be70361ba3
bru170 <80203072+bru170@users.noreply.github.com>
2022-02-01 19:34:06 +0100

    changed to new wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Home`,
-    description: `Kick off yoInfrastructures for partially digital citizens: Supporting informal welfare work in the digitized state `,
+    description: `Kick off your infrastructures for partially digital citizens: Supporting informal welfare work in the digitized state `,
     author: `@antoniabruno`
   },
   plugins: [
@@ -32,7 +32,7 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        url: `https://dev-digital-welfare.pantheonsite.io/graphql`
+        url: `https://admin-soswelfare.space/graphql`
       }
     },
     `gatsby-plugin-styled-components`,
